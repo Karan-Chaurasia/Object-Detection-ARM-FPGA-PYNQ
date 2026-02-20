45488b7e1745344957f85da54ac4f095617e1897
Karan-Chaurasia <karanchaurasia2012@gmail.com>
2026-02-20 22:55:46 +0530

    Implement fpga_process function in fpga_detector.py
    Add FPGA processing function for image frames

--- pynq/fpga_detector.py
+++ pynq/fpga_detector.py
@@ -1,1 +1,23 @@
+from pynq import Overlay, allocate
+import numpy as np
 
+overlay = Overlay("cnn_design_wrapper.bit")
+dma = overlay.axi_dma_0
+
+def fpga_process(frame):
+
+    gray = frame[:,:,0]
+    small = gray[0:64,0:64]
+
+    inbuf = allocate((64,64), dtype=np.int32)
+    outbuf = allocate((64,64), dtype=np.int32)
+
+    inbuf[:] = small
+
+    dma.sendchannel.transfer(inbuf)
+    dma.recvchannel.transfer(outbuf)
+
+    dma.sendchannel.wait()
+    dma.recvchannel.wait()
+
+    return outbuf
