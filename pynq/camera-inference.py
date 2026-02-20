d17c42d9529121cd43e173a4db07674e523e5d01
Karan-Chaurasia <karanchaurasia2012@gmail.com>
2026-02-20 22:56:08 +0530

    Implement YOLO object detection with FPGA preprocessing

--- pynq/camera-inference.py
+++ pynq/camera-inference.py
@@ -1,1 +1,28 @@
+import cv2
+from ultralytics import YOLO
+from fpga_detector import fpga_process
 
+model = YOLO("yolov8m.pt")
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    # FPGA preprocessing
+    fpga_process(frame)
+
+    # YOLO detection
+    results = model(frame)
+
+    annotated = results[0].plot()
+
+    cv2.imshow("ARM+FPGA Object Detection", annotated)
+
+    if cv2.waitKey(1)==27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
