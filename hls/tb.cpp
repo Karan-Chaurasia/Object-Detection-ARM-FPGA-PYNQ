63cf766c6705312da35d9557874a0bf05a8f28e8
Karan-Chaurasia <karanchaurasia2012@gmail.com>
2026-02-20 22:54:50 +0530

    Implement main function for CNN acceleration

--- hls/tb.cpp
+++ hls/tb.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include "cnn_accel.h"
 
+int main(){
+    int in[SIZE][SIZE];
+    int out[SIZE][SIZE];
+
+    for(int i=0;i<SIZE;i++)
+        for(int j=0;j<SIZE;j++)
+            in[i][j]=i+j;
+
+    cnn_accel(in,out);
+
+    std::cout<<"Simulation complete"<<std::endl;
+    return 0;
+}
