f64727c6eef804b88d5818d050eb2bc2de8ea753
Karan-Chaurasia <karanchaurasia2012@gmail.com>
2026-02-20 22:53:55 +0530

    Add header file for CNN acceleration

--- hls/cnn-accel.h
+++ hls/cnn-accel.h
@@ -1,1 +1,11 @@
+#ifndef CNN_ACCEL_H
+#define CNN_ACCEL_H
 
+#include <ap_int.h>
+#include <hls_stream.h>
+
+#define SIZE 64
+
+void cnn_accel(int input[SIZE][SIZE], int output[SIZE][SIZE]);
+
+#endif
